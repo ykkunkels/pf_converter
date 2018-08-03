84f43f4a277a5dabca0f2d663be3adbe2e8e2ad7
Yoram Kunkels <ykkunkels@gmail.com>
2018-08-03 12:03:28 +0200

    added user-defined frequency units

--- Period_Frequency_Converter.R
+++ Period_Frequency_Converter.R
@@ -8,27 +8,47 @@
 ####~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~####
 
 ## Function
-Period_Frequency_Converter <- function(p = NA, f = NA){
+Period_Frequency_Converter <- function(p = NA, f = NA, f_unit = NA){
   
-  ## Sanity check
+  ## Sanity checks
   if(is.na(p) & is.na(f)){
     message("Error: Please enter either a Period- or Frequency value!")
     break()
   }
   
+  if(!is.na(p) & !is.na(f)){
+    message("Error: Please enter either a Period- or Frequency value!")
+    break()
+  }
+  
+
   ## Period unknown but Frequency known (in mHz)
   if(is.na(p) & !is.na(f)){
 
-    p <- round((1 / (f * 60) * 1000), 5)
-    print(paste("Given Frequency:", f, "mHz"))
-    print(paste("Calculated Period: ", round(p, 2), " min ( = ", round((p / 1440), 3), " days )", sep = ""))
-
+    ## Sanity Check
+    if(is.na(f_unit)){
+      message("Error: Please enter Frequency units (either 'mHz' or 'min^(-1)')!")
+      break()
+    }
+    
+    if(f_unit == "mHz"){
+         p <- (1 / (f * 60) * 1000)
+         print(paste("Given Frequency:", f, "mHz"))
+         print(paste("Calculated Period: ", round(p, 2), " min ( = ", round((p / 1440), 3), " days )", sep = ""))
+    }
+    
+    if(f_unit == "min^(-1)"){
+      p <- (1 / f)
+      print(paste("Given Frequency:", f, "min^(-1)"))
+      print(paste("Calculated Period: ", round(p, 2), " min ( = ", round((p / 1440), 3), " days )", sep = ""))
+    }
+    
   }
   
   ## Frequency unknown but Period known (in min)
   if(!is.na(p) & is.na(f)){
     
-    f <- round((((1 / p) / 60) * 1000), 5)
+    f <- (((1 / p) / 60) * 1000)
     print(paste("Given Period:", p, "min"))
     print(paste("Calculated Frequency: ", round(f, 5), " mHz ( = ", round((1 / p), 5), " min^(-1) )", sep = ""))
       
@@ -37,6 +57,7 @@ Period_Frequency_Converter <- function(p = NA, f = NA){
 }
 
 ## Examples
-Period_Frequency_Converter(p = (60 * 24)) # 24h period to frequency
-Period_Frequency_Converter(f = 0.01157) # 0.01157 mHz frequency to period
+Period_Frequency_Converter(f = 0.00069, f_unit = "min^(-1)") # 0.01157 mHz frequency to period
+Period_Frequency_Converter(p = (24 * 60)) # 24h period to frequency
+
  
